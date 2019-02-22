e7b487a6dd6eae62836391a191dafbbd7163dc98
PC <julya.p.dongmail.com>
2019-02-22 20:15:49 +0200

    Uses spring. Types message.

---
+++ SpringTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace SpringTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(IApplicationContext ctx = ContextRegistry.GetContext()){
+                ISimpleMessage simpleMessage = (ISimpleMessage)ctx.GetObject("SimpleMessage");
+                Console.WriteLine(simpleMessage.Message);
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ SpringTest/SimpleMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpringTest
+{
+    class SimpleMessage : ISimpleMessage
+    {
+        private string _message;
+
+        public SimpleMessage()
+        {
+
+        }
+
+        public SimpleMessage(string message)
+        {
+            _message = message;
+        }
+
+        public String Message
+        {
+            get
+            {
+                return _message;
+            }
+
+            set
+            {
+                _message = value;
+            }
+        }
+    }
+}
